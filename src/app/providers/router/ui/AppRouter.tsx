c39736a9a976d5c3b43939a21c31c2548fea0326
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-12-19 18:25:45 +0300

    add private routes

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,26 +1,38 @@
-import { Suspense } from 'react';
+import { getUserAuthData } from 'entities/User';
+import { memo, Suspense, useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/RouteConfig/routeConfig';
 import PageLoader from 'widgets/PageLoader/ui/PageLoader';
 
-const AppRouter = () => (
+const AppRouter = () => {
+    const isAuth = useSelector(getUserAuthData);
 
-    <Routes>
-        {Object.values(routeConfig).map(({ path, element }) => (
-            <Route
-                key={path}
-                path={path}
-                element={(
-                    <Suspense fallback={<PageLoader />}>
-                        <div className="page-wrapper">
-                            {element}
-                        </div>
-                    </Suspense>
-                )}
-            />
-        ))}
-    </Routes>
+    const routes = useMemo(() => Object.values(routeConfig).filter((route) => {
+        if (route.authOnly && !isAuth) {
+            return false;
+        }
 
-);
+        return true;
+    }), [isAuth]);
+    return (
+        <Routes>
+            {routes.map(({ path, element }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    element={(
+                        <Suspense fallback={<PageLoader />}>
+                            <div className="page-wrapper">
+                                {element}
+                            </div>
+                        </Suspense>
+                    )}
+                />
+            ))}
+        </Routes>
 
-export default AppRouter;
+    );
+};
+
+export default memo(AppRouter);
