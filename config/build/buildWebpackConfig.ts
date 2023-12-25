a7396a46e1b7557645c8b3a7d93fd4be32f61a15
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-12-25 12:09:54 +0300

    add articles page

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -12,16 +12,17 @@ export function buildWebpackConfig(
     return {
         mode,
         entry: paths.entry,
-        module: {
-            rules: buildLoaders(options),
-        },
-        resolve: buildResolvers(options),
         output: {
             filename: '[name].[contenthash].js',
             path: paths.build,
             clean: true,
+            publicPath: '/',
         },
         plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(options),
         devtool: isDev ? 'inline-source-map' : undefined,
         devServer: isDev ? buildDevServer(options) : undefined,
     };
