b6de7102e38a70a53e581d8433ba40ce0244c4e2
Katy <115113751+ShidliKaty@users.noreply.github.com>
2024-01-24 13:45:36 +0300

    add helpers for slice, selectors, actions

--- src/entities/Counter/modal/selectors/getCounterValue/getCounterValue.ts
+++ src/entities/Counter/modal/selectors/getCounterValue/getCounterValue.ts
@@ -1,8 +1,14 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { CounterSchema } from '../../types/CounterSchema';
-import { getCounter } from '../getCounter/getCounter';
+// import { createSelector } from '@reduxjs/toolkit';
+// import { CounterSchema } from '../../types/CounterSchema';
+// import { getCounter } from '../getCounter/getCounter';
 
-export const getCounterValue = createSelector(
-    getCounter,
-    (counter: CounterSchema) => counter.value,
+// export const getCounterValue = createSelector(
+//     getCounter,
+//     (counter: CounterSchema) => counter.value,
+// );
+
+import { buildSelector } from '@/shared/lib/store';
+
+export const [useCounterValue, getCounterValue] = buildSelector(
+    (state) => state.counter.value,
 );
--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -2,32 +2,52 @@ import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '@/shared/ui/Button/Button';
 import { getCounterValue } from '../modal/selectors/getCounterValue/getCounterValue';
-import { counterActions } from '../modal/slice/counterSlice';
+import { useCounterActions } from '../modal/slice/counterSlice';
 
 const Counter = () => {
     const dispatch = useDispatch();
     const counterValue = useSelector(getCounterValue);
     const { t } = useTranslation();
 
-    const increment = () => {
-        dispatch(counterActions.increment());
+    const { decrement, increment, add } = useCounterActions();
+
+    const handleInc = () => {
+        increment();
     };
-    const decrement = () => {
-        dispatch(counterActions.decrement());
+
+    const handleDec = () => {
+        decrement();
     };
+
+    const handleAddFive = () => {
+        add(5);
+    };
+
+    // const increment = () => {
+    //     dispatch(counterActions.increment());
+    // };
+    // const decrement = () => {
+    //     dispatch(counterActions.decrement());
+    // };
 
     return (
         <div>
             <h1 data-testid="value-title">{counterValue}</h1>
             <Button
-                onClick={increment}
+                onClick={handleAddFive}
+                data-testid="increment-btn5"
+            >
+                {t('add5')}
+            </Button>
+            <Button
+                onClick={handleInc}
                 data-testid="increment-btn"
             >
                 {t('increment')}
             </Button>
             <Button
-                onClick={decrement}
                 data-testid="decrement-btn"
+                onClick={handleDec}
             >
                 {t('decrement')}
             </Button>
