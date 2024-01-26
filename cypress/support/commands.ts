489302948a814310f21f454720464ca5541fe8e3
Katy <115113751+ShidliKaty@users.noreply.github.com>
2024-01-26 18:40:18 +0300

    add e2e test cases for articles,profile

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,8 +1,6 @@
 // -- This is a child command --s
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
 
-import { login } from './commands/login';
-
 // -- This is a dual command --
 // Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
 
@@ -10,15 +8,18 @@ import { login } from './commands/login';
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 // -- This is a parent command --
+// Cypress.Commands.add('login', login);
 
-Cypress.Commands.add('login', login);
+import * as commonCommands from './commands/common';
+import * as profileCommands from './commands/profile';
+import * as articleCommands from './commands/article';
+import * as commentsCommands from './commands/comments';
+import * as ratingCommands from './commands/rating';
 
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      login(email?: string, password?: string): Chainable<void>
-    }
-  }
-}
+Cypress.Commands.addAll(commonCommands);
+Cypress.Commands.addAll(profileCommands);
+Cypress.Commands.addAll(articleCommands);
+Cypress.Commands.addAll(commentsCommands);
+Cypress.Commands.addAll(ratingCommands);
 
 export {};
